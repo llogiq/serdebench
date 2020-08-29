5ffa78aeeb23dba3e0419b104038f4bdfefd458e
Louis Dureuil <louis.dureuil@xinra.net>
2020-08-29 15:16:30 +0200

    Add capnproto benchmark

--- benches/bench.rs
+++ benches/bench.rs
@@ -6,6 +6,10 @@ use abomonation_derive::Abomonation;
 #[path = "../storeddata_generated.rs"]
 mod flat;
 
+#[allow(dead_code, unused_imports)]
+#[path = "../storeddata_capnp.rs"]
+mod storeddata_capnp;
+
 #[derive(Abomonation, Serialize, Deserialize)]
 pub enum StoredVariants {
     YesNo(bool),
@@ -135,6 +139,76 @@ fn compare_serde(c: &mut Criterion) {
             opt_bool: Some(sd.opt_bool()), //TODO: This isn't actually optional
             vec_strs: sd.vec_strs().unwrap().iter().map(|s| s.into()).collect::<Vec<String>>(),
             range: (range.start() as usize)..(range.end() as usize),
+        }
+    }));
+    group.bench_function("sr.capnproto.unpacked", |b| b.iter(|| {
+        black_box(&mut buffer).clear();
+        let mut message =  ::capnp::message::Builder::new_default();
+        let mut stored_data = message.init_root::<storeddata_capnp::stored_data::Builder>();
+        let mut variant = stored_data.reborrow().init_variant();
+        variant.set_signy(42);
+        let mut strs = stored_data.reborrow().init_vec_strs(1);
+        strs.set(0, "Hello, Rust");
+        let mut opt_bool = stored_data.reborrow().init_opt_bool();
+        opt_bool.set_value(false);
+        let mut range = stored_data.init_range();
+        range.set_start(0);
+        range.set_end(42);
+        capnp::serialize::write_message(black_box(&mut buffer), &message).unwrap();
+    }));
+    println!("capnproto.unpacked: {} bytes", buffer.len());
+    group.bench_function("de.capnproto.unpacked", |b| b.iter(|| {
+        let message_reader = capnp::serialize::read_message(black_box(buffer.as_slice()), Default::default()).unwrap();
+        let sd = message_reader.get_root::<storeddata_capnp::stored_data::Reader>().unwrap();
+        let range = sd.get_range().unwrap();
+        use StoredVariants::*;
+        let variant = match sd.get_variant().which().unwrap() {
+            storeddata_capnp::stored_data::variant::YesNo(yes_no) => YesNo(yes_no),
+            storeddata_capnp::stored_data::variant::Small(small) => Small(small),
+            storeddata_capnp::stored_data::variant::Signy(signy) => Signy(signy),
+            storeddata_capnp::stored_data::variant::Stringy(stringy) => Stringy(stringy.unwrap().into()),
+        };
+        let vec_strs : Vec<String> = sd.get_vec_strs().unwrap().into_iter().map(|v| v.unwrap().into()).collect();
+        StoredData {
+            variant,
+            opt_bool : sd.get_opt_bool().ok().map(|reader| reader.get_value()),
+            vec_strs,
+            range: (range.get_start() as usize)..(range.get_end() as usize),
+        }
+    }));
+    group.bench_function("sr.capnproto.packed", |b| b.iter(|| {
+        black_box(&mut buffer).clear();
+        let mut message =  ::capnp::message::Builder::new_default();
+        let mut stored_data = message.init_root::<storeddata_capnp::stored_data::Builder>();
+        let mut variant = stored_data.reborrow().init_variant();
+        variant.set_signy(42);
+        let mut strs = stored_data.reborrow().init_vec_strs(1);
+        strs.set(0, "Hello, Rust");
+        let mut opt_bool = stored_data.reborrow().init_opt_bool();
+        opt_bool.set_value(false);
+        let mut range = stored_data.init_range();
+        range.set_start(0);
+        range.set_end(42);
+        capnp::serialize_packed::write_message(black_box(&mut buffer), &message).unwrap();
+    }));
+    println!("capnproto.packed: {} bytes", buffer.len());
+    group.bench_function("de.capnproto.packed", |b| b.iter(|| {
+        let message_reader = capnp::serialize_packed::read_message(black_box(buffer.as_slice()), Default::default()).unwrap();
+        let sd = message_reader.get_root::<storeddata_capnp::stored_data::Reader>().unwrap();
+        let range = sd.get_range().unwrap();
+        use StoredVariants::*;
+        let variant = match sd.get_variant().which().unwrap() {
+            storeddata_capnp::stored_data::variant::YesNo(yes_no) => YesNo(yes_no),
+            storeddata_capnp::stored_data::variant::Small(small) => Small(small),
+            storeddata_capnp::stored_data::variant::Signy(signy) => Signy(signy),
+            storeddata_capnp::stored_data::variant::Stringy(stringy) => Stringy(stringy.unwrap().into()),
+        };
+        let vec_strs : Vec<String> = sd.get_vec_strs().unwrap().into_iter().map(|v| v.unwrap().into()).collect();
+        StoredData {
+            variant,
+            opt_bool : sd.get_opt_bool().ok().map(|reader| reader.get_value()),
+            vec_strs,
+            range: (range.get_start() as usize)..(range.get_end() as usize),
         }
     }));
     group.bench_function("sr.abomonation", |b| b.iter(|| {
